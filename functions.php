0393a3554edb046d900cea4ef2559c3a8fc0eaec
Perulou <mka.o1986@gmail.com>
2025-08-28 13:49:04 +0900

    ボタンのバリエーションの追加

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
 function my_block_assets(){
-  wp_enqueue_style('morning-start',get_template_directory_uri().'/assets/css/style.css',array(),filemtime( get_theme_file_path('/assets/css/style.css')), 'all');
+  wp_enqueue_style( 'morning-start', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime( get_theme_file_path('/assets/css/style.css')), 'all');
 }
 add_action( 'enqueue_block_assets', 'my_block_assets');
+
+function add_default_arrow_button(){
+  register_block_style(
+    'core/button',
+    array(
+      'name' => 'defalt-arrow-button',
+      'label' => '矢印付き(Default)'
+    )
+  );
+}
+add_action( 'init', 'add_default_arrow_button' );
+
+
+function add_priority_arrow_button(){
+  register_block_style(
+    'core/button',
+    array(
+      'name' => 'priority-arrow-button',
+      'label' => '矢印付き(Priority)'
+    )
+  );
+}
+add_action( 'init', 'add_priority_arrow_button' );
